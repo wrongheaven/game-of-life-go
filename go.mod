3877106b5f9f0720a69500267073dcf6e2265b45
Henrik Engebretsen <wrongheaven73@gmail.com>
2024-07-30 04:37:44 +0200

    Embed icon

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.22.2
 require github.com/hajimehoshi/ebiten v1.12.12
 
 require (
-	github.com/Kodeworks/golang-image-ico v0.0.0-20141118225523-73f0f4cfade9 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.0.0-20200801110659-972c09e46d76 // indirect
--- main.go
+++ main.go
@@ -9,22 +9,23 @@ import (
 	"image/png"
 
 	eb "github.com/hajimehoshi/ebiten"
-	"github.com/wrongheaven/game-of-life-go/utils"
 )
 
 //go:embed assets/icon.png
 var iconData []byte
 
 const (
-	WIDTH        = 400
-	HEIGHT       = 400
+	WIDTH        = 1280
+	HEIGHT       = 720
 	FPS          = 10
 	WINDOW_TITLE = "hello mother"
 )
 
 func main() {
 	iconImage, err := png.Decode(bytes.NewReader(iconData))
-	utils.Bekind(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	eb.SetWindowIcon([]image.Image{iconImage})
 
